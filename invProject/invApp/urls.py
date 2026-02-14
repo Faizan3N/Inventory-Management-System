894aea4e18379c16ef7cd0085ee11c9d2c7fc4ac
Faizan Ali <faizanali3n@gmail.com>
2026-02-14 17:01:38 +0500

    Completed Project

--- invProject/invApp/urls.py
+++ invProject/invApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.home_view, name='home'),
+   path('list/', views.product_list_view, name='product-list'),
    path('create/', views.product_create_view, name='create_product'),
    path('update/<int:product_id>/', views.product_update_view, name='update_product'),
    path('delete/<int:product_id>/', views.product_delete_view, name='delete_product'),
