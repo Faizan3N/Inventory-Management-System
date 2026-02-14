d3ba798f65ab55712ade6fc9384b335096bb3834
Faizan Ali <faizanali3n@gmail.com>
2026-02-14 15:39:13 +0500

    Updated

--- invProject/invApp/migrations/forms.py
+++ invProject/invApp/migrations/forms.py
@@ -4,11 +4,26 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'sku', 'price', 'quantity', 'supplier']
+        fields = '__all__'
+        labels = {
+            'product_id': 'Product ID',
+            'name': 'Name',
+            'sku': 'SKU',
+            'price': 'Price',
+            'quantity': 'Quantity',
+            'supplier': 'Supplier',
+        }
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'sku': forms.TextInput(attrs={'class': 'form-control'}),
-            'price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'supplier': forms.TextInput(attrs={'class': 'form-control'}),
+            'product_id': forms.NumberInput
+            (attrs={'placeholder': 'e.g. 1', 'class':'form-control'}),
+            'name': forms.TextInput(
+                attrs={'placeholder': 'e.g. shirt', 'class':'form-control'}),
+            'sku': forms.TextInput(
+                attrs={'placeholder': 'e.g. SHRT-001', 'class':'form-control'}),
+            'price': forms.NumberInput(
+                attrs={'placeholder': 'e.g. 19.99', 'class':'form-control'}),
+            'quantity': forms.NumberInput(
+                attrs={'placeholder': 'e.g. 100', 'class':'form-control'}),
+            'supplier': forms.TextInput(
+                attrs={'placeholder': 'e.g. ABC Supplier', 'class':'form-control'}),    
         }
